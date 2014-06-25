c18bfdd2230e6c7cbf04db2b4ca87c7e4bc7d97d
masatokg <masatokg@Audrey_Tautou>
2014-06-26 08:04:12 +0900

    4th Commit

---
+++ src/jp/ac/st/asojuku/layouttraining001/GridLayoutActivity.java
@@ -0,0 +1,21 @@
+package jp.ac.st.asojuku.layouttraining001;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GridLayoutActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.gridlayout_activity);
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onResume();
+	}
+
+}
